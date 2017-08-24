0aafd7fb21a7de658a6eb1e4d885b2677cdc28ef
Ranga Rao Karanam <rangaraokaranam@Rangas-MacBook-Pro.local>
2017-08-24 09:47:49 +0530

    Next Step

--- 02-jpa-advanced/src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Course.java
+++ 02-jpa-advanced/src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Course.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
--- 02-jpa-advanced/src/test/java/com/in28minutes/jpa/hibernate/demo/repository/CourseRepositoryTest.java
+++ 02-jpa-advanced/src/test/java/com/in28minutes/jpa/hibernate/demo/repository/CourseRepositoryTest.java
@@ -3,6 +3,8 @@ package com.in28minutes.jpa.hibernate.demo.repository;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
+import javax.persistence.EntityManager;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -11,9 +13,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.in28minutes.jpa.hibernate.demo.DemoApplication;
 import com.in28minutes.jpa.hibernate.demo.entity.Course;
+import com.in28minutes.jpa.hibernate.demo.entity.Review;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = DemoApplication.class)
@@ -23,6 +27,10 @@ public class CourseRepositoryTest {
 
 	@Autowired
 	CourseRepository repository;
+	
+	@Autowired
+	EntityManager em;
+	
 
 	@Test
 	public void findById_basic() {
@@ -59,4 +67,26 @@ public class CourseRepositoryTest {
 		repository.playWithEntityManager();
 	}
 	
+	@Test
+	@Transactional
+	public void retrieveReviewsForCourse() {
+		Course course = repository.findById(10001L);
+		logger.info("{}",course.getReviews());
+	}
+
+	@Test
+	@Transactional
+	public void retrieveCourseForReview() {
+		Review review = em.find(Review.class, 50001L);
+		logger.info("{}",review.getCourse());
+	}
+
 }
+
+
+
+
+
+
+
+
