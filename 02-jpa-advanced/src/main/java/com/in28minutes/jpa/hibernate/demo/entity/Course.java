e1bf4dd44aab041ae5b273180d152345fa4c35de
Ranga Rao Karanam <rangaraokaranam@Rangas-MacBook-Pro.local>
2017-08-24 10:09:04 +0530

    Next Step

--- 02-jpa-advanced/src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Course.java
+++ 02-jpa-advanced/src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Course.java
@@ -6,9 +6,9 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
@@ -34,6 +34,9 @@ public class Course {
 
 	@OneToMany(mappedBy="course")
 	private List<Review> reviews = new ArrayList<>();
+	
+	@ManyToMany
+	private List<Student> students = new ArrayList<>();
 	
 	@UpdateTimestamp
 	private LocalDateTime lastUpdatedDate;
@@ -67,6 +70,14 @@ public class Course {
 
 	public void removeReview(Review review) {
 		this.reviews.remove(review);
+	}
+
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void addStudent(Student student) {
+		this.students.add(student);
 	}
 
 	public Long getId() {
--- 02-jpa-advanced/src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Student.java
+++ 02-jpa-advanced/src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Student.java
@@ -1,10 +1,14 @@
 package com.in28minutes.jpa.hibernate.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -19,6 +23,9 @@ public class Student {
 	
 	@OneToOne(fetch=FetchType.LAZY)
 	private Passport passport;
+	
+	@ManyToMany
+	private List<Course> courses = new ArrayList<>();
 
 	protected Student() {
 	}
@@ -41,6 +48,14 @@ public class Student {
 
 	public void setPassport(Passport passport) {
 		this.passport = passport;
+	}
+	
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void addCourse(Course course) {
+		this.courses.add(course);
 	}
 
 	public Long getId() {
