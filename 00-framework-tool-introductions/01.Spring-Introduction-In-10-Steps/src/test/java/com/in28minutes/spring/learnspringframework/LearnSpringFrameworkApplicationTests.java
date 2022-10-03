4fa719de54089e5faa9c63f0d4713d110b27c157
Ranga Karanam <in28minutes@gmail.com>
2022-10-03 13:17:48 +0530

    Update to Spring Boot 3

---
+++ 00-framework-tool-introductions/01.Spring-Introduction-In-10-Steps/src/test/java/com/in28minutes/spring/learnspringframework/LearnSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.spring.learnspringframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
